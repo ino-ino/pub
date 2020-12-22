1119e1f2146c5c2a82d8916367626d32babdf21e
“ino-ino” <ino_007@icloud.com>
2020-12-22 02:02:18 +0000

    fix posts layouts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,38 @@
-<h1>Posts</h1>
+@extends('layouts.layouts')
 
-@foreach($posts as $post)
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+@section('title', 'Simple Board')
+
+@section('content')
+
+    @if (session('message'))
+        {{ session('message') }}
+    @endif
+
+    <h1>Posts</h1>
+
+    @foreach($posts as $post)
+    
+     <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">{{ $post->content }}</p>
+                
+                 <div class="d-flex" style="height: 36.4px;">
+                    <a href="/posts/{{ $post->id }}" class="btn btn-outline-primary">Show</a>
+                    <a href="/posts/{{ $post->id }}/edit" class="btn btn-outline-primary">Edit</a>
+                    <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
+                        <input type="hidden" name="_method" value="DELETE">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <button type="submit" class="btn btn-outline-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+     
+        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->id }}/edit">Edit</a>
+        
 @endforeach
 
 <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
@@ -12,3 +42,5 @@
     </form>
 
 <a href="/posts/create">New Post</a>
+
+@endsection
