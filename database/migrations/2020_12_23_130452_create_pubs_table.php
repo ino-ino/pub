e68e8a79fa3372c1cef3c95b6caf23de36df4df9
“ino-ino” <ino_007@icloud.com>
2020-12-23 13:33:34 +0000

    generate model & controller pub

--- database/migrations/2020_12_23_130452_create_pubs_table.php
+++ database/migrations/2020_12_23_130452_create_pubs_table.php
@@ -16,7 +16,7 @@ class CreatePubsTable extends Migration
         Schema::create('pubs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string("name");
-            $table->string("url");        
+                    
             $table->timestamps();
         });
     }
